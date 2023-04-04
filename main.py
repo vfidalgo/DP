aa7801d19535deb0a590403a55e0c1d53474d9a8
vfidalgo <dongash@gmail.com>
2023-04-04 22:51:18 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,25 @@
+from metabuscador import Metabuscador
+from reporte import Reporte
+from base_datos import BaseDatos
+from web import app
+
+def main():
+    # Configuración de la base de datos
+    bd = BaseDatos()
+
+    # Configuración del metabuscador
+    mb = Metabuscador()
+
+    # Configuración del generador de reportes
+    rpt = Reporte()
+
+    # Configuración de la aplicación web
+    app.config['BD'] = bd
+    app.config['MB'] = mb
+    app.config['RPT'] = rpt
+
+    # Inicio de la aplicación web
+    app.run()
+
+if __name__ == '__main__':
+    main()
