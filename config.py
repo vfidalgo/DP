a984f3c5799afdf24404caf54514560508df2fb3
vfidalgo <dongash@gmail.com>
2023-04-04 23:17:02 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,9 @@
+# Configuración para la conexión a la base de datos MongoDB
+MONGO_HOST = 'localhost'
+MONGO_PORT = 27017
+MONGO_DBNAME = 'nombre_de_la_base_de_datos'
+MONGO_USERNAME = 'nombre_de_usuario'
+MONGO_PASSWORD = 'contraseña_del_usuario'
+
+# Configuración para la API de Shodan
+SHODAN_API_KEY = 'clave_de_api_de_shodan'
